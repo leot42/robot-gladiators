238c28be28198fb57043b26aed0cc46d7de0b042
townsendlionel <townsendlionel@gmail.com>
2020-12-14 12:15:23 -0600

    used js to communicate from the window to the log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("Hello world!");
+let playerName;
+
+function fight(playerName) {
+    console.log(`The Fight Of ${playerName} Has Begun!`);
+}
+
+playerName = window.prompt("What's the robot name?");
+
+fight(playerName);
